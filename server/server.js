eca2a59ba8569e6f618b617c01f22110b5b17b83
garyhui <gary.hui@sendum.com>
2023-09-20 10:40:29 -0700

    Restaurant routes created. Still need to implement functionality

--- server/server.js
+++ server/server.js
@@ -1,28 +1,34 @@
-// import express
 import express from 'express'
 import 'dotenv/config'
-// give instance of express app
+import morgan from 'morgan'
 const app = express()
+
+// Middleware = some function that is between the frontend request and the backend response  (It performs something with the request).... route handler is technically also middleware
+app.use(morgan('dev')) // morgan for useful logging
+app.use(express.json()) // !! Middleware express.json is useful  enabling "req.body" to return a
+
+/**
+ * CRUD operations for Restaurants: api/v1/restaurants/:restaurantId
+ * - get all restaurants
+ * - get one restaurant :restaurantId
+ * - create restaurant
+ * - delete restaurant /:restaurantId
+ * - update restaurant /:restaurantId
+ */
+
 // make app listen at port
 const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log('Server is running on port:', port)
 })
 
-// Middleware = some function that is between the frontend request and the backend response  (It performs something with the request).... route handler is technically also middleware
-
-/**
- * CRUD operations for Restaurants: api/v1/restaurants/:id
- * - get all restaurants
- * - get one restaurant :id
- * - create restaurant
- * - delete restaurant /:id
- * - update restaurant /:id
- */
+// GET one restaurant
+app.get('/api/v1/restaurants/:restaurantId', (req, res) => {
+  console.log('get one')
+})
 
 // GET all restaurants
 app.get('/api/v1/restaurants', (req, res) => {
-  console.log('test')
   res.status(200).json({
     status: 'successs',
     data: {
@@ -31,8 +37,16 @@ app.get('/api/v1/restaurants', (req, res) => {
   })
 })
 
-// GET one restaurant
-app.get('api/v1/restaurants/:id', (req, res) => {
-  console.log(req.params)
-  res.send('get one ')
+// CREATE a restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+  console.log('create rest')
+  res.send('create ')
+})
+
+// DELETE a restaurant
+app.delete('/api/v1/restaurants/:restaurantId', (req, res) => {
+  // find restaurant with id
+  // if found --> delete
+  console.log('delete')
+  res.send('delete')
 })
