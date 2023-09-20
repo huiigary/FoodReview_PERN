6acc82ffdcdc842ea206287c35c2296eeabea463
garyhui <gary.hui@sendum.com>
2023-09-19 23:28:11 -0700

    Wrote down CRUD operations to implement

--- server/server.js
+++ server/server.js
@@ -9,9 +9,30 @@ app.listen(port, () => {
   console.log('Server is running on port:', port)
 })
 
-app.get('/getRestaurants', (req, res) => {
+// Middleware = some function that is between the frontend request and the backend response  (It performs something with the request).... route handler is technically also middleware
+
+/**
+ * CRUD operations for Restaurants: api/v1/restaurants/:id
+ * - get all restaurants
+ * - get one restaurant :id
+ * - create restaurant
+ * - delete restaurant /:id
+ * - update restaurant /:id
+ */
+
+// GET all restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+  console.log('test')
   res.status(200).json({
     status: 'successs',
-    restaurant: 'test restaurant',
+    data: {
+      restaurants: ['Mcdonalds', 'testRestaurant', 'Pho quyen'],
+    },
   })
+})
+
+// GET one restaurant
+app.get('api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params)
+  res.send('get one ')
 })
