d63f164949baa3db146ecc8f63dab3d3264c5761
garyhui <gary.hui@sendum.com>
2023-10-13 14:17:30 -0700

    Created Reviews table that references restaurants as foreign key

--- server/db.sql
+++ server/db.sql
@@ -13,3 +13,14 @@ CREATE TABLE restaurants (
 
 INSERT INTO restaurants( name, location, price_range) values ( 'Test Restaurant Name', 'Vancouver', 25);
 INSERT INTO restaurants( name, location, price_range) values ( 'TestName2', 'Vancouver', 30);
+
+CREATE TABLE reviews ( 
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    restaurant_ID BIGINT NOT NULL REFERENCES restaurants (id),
+    name VARCHAR(100) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check (rating >=1 AND rating <=5)
+);
+INSERT INTO reviews (name, review, rating) values ('test name', 'test review', 1);
+INSERT INTO reviews (name, review, rating) values ('test name2', 'test review2', 2);
+-- ALTER TABLE reviews ALTER COLUMN ratings SET NOT NULL // used to alter column
